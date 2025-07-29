9b85d69147be80921c4a0810ba53255ea4ab1872
Pranit003 <88771350+Pranit003@users.noreply.github.com>
2025-07-29 12:21:34 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,19 @@
-package com.example;
+package com.mycompany.app;
 
+/**
+ * Hello world!
+ */
 public class App {
+    private final String message = "Hello World!";
+
+    public App() {}
+
+    public String getMessage() {
+        return message;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(new App().getMessage());
     }
 }
+
